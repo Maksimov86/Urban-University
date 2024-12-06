9460c05246e86a94a114228d2545e4bfb91d952e
Maksimov86 <tylerbk666@gmail.com>
2024-12-06 11:13:25 +0300

    Remove embedded submodule 'Introductory module'

---
+++ Module_1/module_1_3_0.py
@@ -0,0 +1,8 @@
+name = "Vyacheslav"
+print(f'name: {name}')
+age = 38
+print(f'age: {age}')
+age = 38 + 1
+print(f'new age: {age}')
+is_student = True
+print(f'is student: {is_student}')
---
+++ Module_1/module_1_3_2.py
@@ -0,0 +1,6 @@
+example = 'Lorem ipsum dolor sit amet'
+print(example[0])
+print(example[-1])
+print(example[13:])
+print(example[::-1])
+print(example[::2])
